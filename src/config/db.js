59f77b9b0d916348b8a44d31c497ee7e0c427e1b
lukas-ig <lukas.rocha@irmaosgoncalves.com.br>
2022-11-10 10:20:16 -0400

    Correções na documentação de apresentação

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,17 @@
 module.exports = {
-    client: process.env.DB_CLIENT,
+    client: "mysql",
     connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      user: process.env.DB_USERNAME
+    },
+    timezone: 'UTC',
+    migrations: {
+      tableName: "knex_migrations"
+    },
+    pool: {
+      max: 10,
+      min: 2
     }
-}
+  };
